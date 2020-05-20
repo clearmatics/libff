8445fa06684101896383433939a054a313d4d939
Antoine Rondelet <rondelet.antoine@gmail.com>
2020-05-20 17:56:34 +0100

    Added test case for bls12_377 in bilinearity test

--- libff/algebra/curves/tests/test_bilinearity.cpp
+++ libff/algebra/curves/tests/test_bilinearity.cpp
@@ -12,6 +12,7 @@
 #include <libff/algebra/curves/alt_bn128/alt_bn128_pp.hpp>
 #include <libff/algebra/curves/mnt/mnt4/mnt4_pp.hpp>
 #include <libff/algebra/curves/mnt/mnt6/mnt6_pp.hpp>
+#include <libff/algebra/curves/bls12_377/bls12_377_pp.hpp>
 
 using namespace libff;
 
@@ -127,6 +128,10 @@ int main(void)
     alt_bn128_pp::init_public_params();
     pairing_test<alt_bn128_pp>();
     double_miller_loop_test<alt_bn128_pp>();
+
+    bls12_377_pp::init_public_params();
+    pairing_test<bls12_377_pp>();
+    double_miller_loop_test<bls12_377_pp>();
 
 #ifdef CURVE_BN128       // BN128 has fancy dependencies so it may be disabled
     bn128_pp::init_public_params();
