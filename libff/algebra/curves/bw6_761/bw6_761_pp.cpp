435addbc7d89db27d197c2d23406b00856dc1a2a
Duncan Tebbs <duncan.tebbs@gmail.com>
2020-06-05 10:09:33 +0100

    bw6-761: fix interface inconsistency (supports generic client code)

--- libff/algebra/curves/bw6_761/bw6_761_pp.cpp
+++ libff/algebra/curves/bw6_761/bw6_761_pp.cpp
@@ -17,36 +17,31 @@ bw6_761_G1_precomp bw6_761_pp::precompute_G1(const bw6_761_G1 &P)
     return bw6_761_precompute_G1(P);
 }
 
-bw6_761_G2_precomp bw6_761_pp::precompute_G2(const bw6_761_G2& Q, const bigint<bw6_761_Fq::num_limbs> &loop_count)
+bw6_761_G2_precomp bw6_761_pp::precompute_G2(const bw6_761_G2& Q)
 {
-    return bw6_761_precompute_G2(Q, loop_count);
+    return bw6_761_precompute_G2(Q);
 }
 
 bw6_761_Fq6 bw6_761_pp::miller_loop(const bw6_761_G1_precomp &prec_P,
-                              const bw6_761_G2_precomp &prec_Q_1,
-                              const bw6_761_G2_precomp &prec_Q_2)
+                                    const bw6_761_G2_precomp &prec_Q)
 {
-    return bw6_761_miller_loop(prec_P, prec_Q_1, prec_Q_2);
+    return bw6_761_miller_loop(prec_P, prec_Q);
 }
 
 bw6_761_Fq6 bw6_761_pp::double_miller_loop(const bw6_761_G1_precomp &prec_P1,
-                                   const bw6_761_G2_precomp &prec_Q1_1,
-                                   const bw6_761_G2_precomp &prec_Q2_1,
-                                   const bw6_761_G1_precomp &prec_P2,
-                                   const bw6_761_G2_precomp &prec_Q1_2,
-                                   const bw6_761_G2_precomp &prec_Q2_2)
+                                           const bw6_761_G2_precomp &prec_Q1,
+                                           const bw6_761_G1_precomp &prec_P2,
+                                           const bw6_761_G2_precomp &prec_Q2)
 {
-    return bw6_761_double_miller_loop(prec_P1, prec_Q1_1, prec_Q2_1, prec_P2, prec_Q1_2, prec_Q2_2);
+    return bw6_761_double_miller_loop(prec_P1, prec_Q1, prec_P2, prec_Q2);
 }
 
-bw6_761_Fq6 bw6_761_pp::pairing(const bw6_761_G1 &P,
-                          const bw6_761_G2 &Q)
+bw6_761_Fq6 bw6_761_pp::pairing(const bw6_761_G1 &P, const bw6_761_G2 &Q)
 {
     return bw6_761_pairing(P, Q);
 }
 
-bw6_761_Fq6 bw6_761_pp::reduced_pairing(const bw6_761_G1 &P,
-                                  const bw6_761_G2 &Q)
+bw6_761_Fq6 bw6_761_pp::reduced_pairing(const bw6_761_G1 &P, const bw6_761_G2 &Q)
 {
     return bw6_761_reduced_pairing(P, Q);
 }
