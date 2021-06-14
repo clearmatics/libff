9e212ddfcf0c688e6bcfa733c9ea648e4506009b
Duncan Tebbs <duncan.tebbs@gmail.com>
2021-06-14 13:40:03 +0100

    profile group mixed_add

--- libff/algebra/curves/profile/algebra_groups_profile.cpp
+++ libff/algebra/curves/profile/algebra_groups_profile.cpp
@@ -12,35 +12,74 @@
 
 using namespace libff;
 
+/// Number of random elements to generate (since this process is expensive)
+static const size_t ADD_NUM_DIFFERENT_ELEMENTS = 1024;
+
+/// Number of element to operate on in a single iteration.
+static const size_t ADD_NUM_ELEMENTS = 1024 * 1024;
+
 template<typename GroupT> bool profile_group_add()
 {
-    static const size_t NUM_DIFFERENT_ELEMENTS = 1024;
-    static const size_t NUM_ELEMENTS = 1024 * 1024;
-
     std::vector<GroupT> elements;
     {
-        elements.reserve(NUM_ELEMENTS);
+        elements.reserve(ADD_NUM_ELEMENTS);
         size_t i = 0;
-        for (; i < NUM_DIFFERENT_ELEMENTS; ++i) {
+        for (; i < ADD_NUM_DIFFERENT_ELEMENTS; ++i) {
             elements.push_back(GroupT::random_element());
         }
-        for (; i < NUM_ELEMENTS; ++i) {
-            elements.push_back(elements[i % NUM_DIFFERENT_ELEMENTS]);
+        for (; i < ADD_NUM_ELEMENTS; ++i) {
+            elements.push_back(elements[i % ADD_NUM_DIFFERENT_ELEMENTS]);
         }
     }
 
-    std::cout << "    num elements: " << std::to_string(NUM_ELEMENTS) << "\n";
+    std::cout << "    num elements: " << std::to_string(ADD_NUM_ELEMENTS)
+              << "\n";
 
     size_t num_elements = 0;
     GroupT accum = GroupT::zero();
     enter_block("group add operation profiling");
     for (const GroupT &el : elements) {
-        accum = accum + el;
+        accum = accum.add(el);
         num_elements++;
     }
     leave_block("group add operation profiling");
 
-    if (num_elements != NUM_ELEMENTS) {
+    if (num_elements != ADD_NUM_ELEMENTS) {
+        throw std::runtime_error("invalid number of elements seen");
+    }
+
+    return true;
+}
+
+template<typename GroupT> bool profile_group_mixed_add()
+{
+    std::vector<GroupT> elements;
+    {
+        elements.reserve(ADD_NUM_ELEMENTS);
+        size_t i = 0;
+        for (; i < ADD_NUM_DIFFERENT_ELEMENTS; ++i) {
+            GroupT e = GroupT::random_element();
+            e.to_affine_coordinates();
+            elements.push_back(e);
+        }
+        for (; i < ADD_NUM_ELEMENTS; ++i) {
+            elements.push_back(elements[i % ADD_NUM_DIFFERENT_ELEMENTS]);
+        }
+    }
+
+    std::cout << "    num elements: " << std::to_string(ADD_NUM_ELEMENTS)
+              << "\n";
+
+    size_t num_elements = 0;
+    GroupT accum = GroupT::one();
+    enter_block("group mixed add operation profiling");
+    for (const GroupT &el : elements) {
+        accum = accum.mixed_add(el);
+        num_elements++;
+    }
+    leave_block("group mixed add operation profiling");
+
+    if (num_elements != ADD_NUM_ELEMENTS) {
         throw std::runtime_error("invalid number of elements seen");
     }
 
@@ -82,8 +121,18 @@ int main(void)
         throw std::runtime_error("failed");
     }
 
+    std::cout << "  profile_group_mixed_add<alt_bn128_G1>:\n";
+    if (!profile_group_mixed_add<alt_bn128_G1>()) {
+        throw std::runtime_error("failed");
+    }
+
     std::cout << "  profile_group_add<alt_bn128_G2>:\n";
     if (!profile_group_add<alt_bn128_G2>()) {
+        throw std::runtime_error("failed");
+    }
+
+    std::cout << "  profile_group_mixed_add<alt_bn128_G2>:\n";
+    if (!profile_group_mixed_add<alt_bn128_G2>()) {
         throw std::runtime_error("failed");
     }
 
@@ -92,6 +141,11 @@ int main(void)
 
     std::cout << "  profile_group_add<bls12_377_G1>:\n";
     if (!profile_group_add<bls12_377_G1>()) {
+        throw std::runtime_error("failed");
+    }
+
+    std::cout << "  profile_group_mixed_add<bls12_377_G1>:\n";
+    if (!profile_group_mixed_add<bls12_377_G1>()) {
         throw std::runtime_error("failed");
     }
 
