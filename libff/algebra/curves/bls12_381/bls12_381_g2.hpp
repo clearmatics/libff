a625d65e78c8a9d5f05439197d010069d769edda
Vesselin Velichkov <vesko@lancre>
2022-03-10 11:35:36 +0000

    bls12-381 migration from scipr-lab: replaced ceil_size_in_bits with size_in_bits

--- libff/algebra/curves/bls12_381/bls12_381_g2.hpp
+++ libff/algebra/curves/bls12_381/bls12_381_g2.hpp
@@ -73,7 +73,8 @@ public:
     static bls12_381_G2 one();
     static bls12_381_G2 random_element();
 
-    static std::size_t size_in_bits() { return twist_field::ceil_size_in_bits() + 1; }
+  //    static std::size_t size_in_bits() { return twist_field::ceil_size_in_bits() + 1; } // from scipr-lab (VV)
+    static std::size_t size_in_bits() { return twist_field::size_in_bits() + 1; }
     static bigint<base_field::num_limbs> field_char() { return base_field::field_char(); }
     static bigint<scalar_field::num_limbs> order() { return scalar_field::field_char(); }
 
