fe06d9e4ed84256b73a713b468e1633d6b196fe1
Vesselin Velichkov <vesko@lancre>
2022-03-15 11:21:00 +0000

    bls12-381: formatting

--- libff/algebra/fields/fp6_3over2.hpp
+++ libff/algebra/fields/fp6_3over2.hpp
@@ -37,13 +37,13 @@ public:
     typedef Fp2_model<n, modulus> my_Fp2;
 
     // from scipr-lab (VV)
-    static bigint<6*n> euler; // (modulus^6-1)/2 
-    static std::size_t s; // modulus^6 = 2^s * t + 1
-    static bigint<6*n> t; // with t odd
-    static bigint<6*n> t_minus_1_over_2; // (t-1)/2
+    static bigint<6 * n> euler;              // (modulus^6-1)/2
+    static std::size_t s;                    // modulus^6 = 2^s * t + 1
+    static bigint<6 * n> t;                  // with t odd
+    static bigint<6 * n> t_minus_1_over_2;   // (t-1)/2
     static Fp6_3over2_model<n, modulus> nqr; // a quadratic nonresidue in Fp6
     static Fp6_3over2_model<n, modulus> nqr_to_t; // nqr^t
-   
+
     static my_Fp2 non_residue;
     /// non_residue^((modulus^i-1)/3)   for i=0,1,2,3,4,5
     static my_Fp2 Frobenius_coeffs_c1[6];
@@ -126,23 +126,23 @@ template<mp_size_t n, const bigint<n> &modulus>
 Fp6_3over2_model<n, modulus> operator*(
     const Fp2_model<n, modulus> &lhs, const Fp6_3over2_model<n, modulus> &rhs);
 
-// bls12-381 from scipr-lab (VV)  
-template<mp_size_t n, const bigint<n>& modulus>
-bigint<6*n> Fp6_3over2_model<n, modulus>::euler;
+// bls12-381 from scipr-lab (VV)
+template<mp_size_t n, const bigint<n> &modulus>
+bigint<6 * n> Fp6_3over2_model<n, modulus>::euler;
 
-template<mp_size_t n, const bigint<n>& modulus>
+template<mp_size_t n, const bigint<n> &modulus>
 size_t Fp6_3over2_model<n, modulus>::s;
 
-template<mp_size_t n, const bigint<n>& modulus>
-bigint<6*n> Fp6_3over2_model<n, modulus>::t;
+template<mp_size_t n, const bigint<n> &modulus>
+bigint<6 * n> Fp6_3over2_model<n, modulus>::t;
 
-template<mp_size_t n, const bigint<n>& modulus>
-bigint<6*n> Fp6_3over2_model<n, modulus>::t_minus_1_over_2;
+template<mp_size_t n, const bigint<n> &modulus>
+bigint<6 * n> Fp6_3over2_model<n, modulus>::t_minus_1_over_2;
 
-template<mp_size_t n, const bigint<n>& modulus>
+template<mp_size_t n, const bigint<n> &modulus>
 Fp6_3over2_model<n, modulus> Fp6_3over2_model<n, modulus>::nqr;
 
-template<mp_size_t n, const bigint<n>& modulus>
+template<mp_size_t n, const bigint<n> &modulus>
 Fp6_3over2_model<n, modulus> Fp6_3over2_model<n, modulus>::nqr_to_t;
 
 template<mp_size_t n, const bigint<n> &modulus>
