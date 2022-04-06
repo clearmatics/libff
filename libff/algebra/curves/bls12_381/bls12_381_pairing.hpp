0d679d9e8d2c57c72e62565164eff00c46491759
Vesselin Velichkov <vesko@lancre>
2022-04-06 16:11:42 +0100

    bls12-381: formatting

--- libff/algebra/curves/bls12_381/bls12_381_pairing.hpp
+++ libff/algebra/curves/bls12_381/bls12_381_pairing.hpp
@@ -54,9 +54,14 @@ struct bls12_381_ate_G2_precomp {
         std::istream &in, bls12_381_ate_G2_precomp &prec_Q);
 };
 
-void bls12_381_doubling_step_for_miller_loop(const bls12_381_Fq two_inv, bls12_381_G2 &current, bls12_381_ate_ell_coeffs &c);
-void bls12_381_mixed_addition_step_for_miller_loop(const bls12_381_G2 base, bls12_381_G2 &current, bls12_381_ate_ell_coeffs &c);
-
+void bls12_381_doubling_step_for_miller_loop(
+    const bls12_381_Fq two_inv,
+    bls12_381_G2 &current,
+    bls12_381_ate_ell_coeffs &c);
+void bls12_381_mixed_addition_step_for_miller_loop(
+    const bls12_381_G2 base,
+    bls12_381_G2 &current,
+    bls12_381_ate_ell_coeffs &c);
 
 bls12_381_ate_G1_precomp bls12_381_ate_precompute_G1(const bls12_381_G1 &P);
 bls12_381_ate_G2_precomp bls12_381_ate_precompute_G2(const bls12_381_G2 &Q);
@@ -70,9 +75,11 @@ bls12_381_Fq12 bls12_381_ate_double_miller_loop(
     const bls12_381_ate_G1_precomp &prec_P2,
     const bls12_381_ate_G2_precomp &prec_Q2);
 
-bls12_381_Fq12 bls12_381_final_exponentiation_first_chunk(const bls12_381_Fq12 &elt);
+bls12_381_Fq12 bls12_381_final_exponentiation_first_chunk(
+    const bls12_381_Fq12 &elt);
 bls12_381_Fq12 bls12_381_exp_by_z(const bls12_381_Fq12 &elt);
-bls12_381_Fq12 bls12_381_final_exponentiation_last_chunk(const bls12_381_Fq12 &elt);
+bls12_381_Fq12 bls12_381_final_exponentiation_last_chunk(
+    const bls12_381_Fq12 &elt);
 
 bls12_381_Fq12 bls12_381_ate_pairing(
     const bls12_381_G1 &P, const bls12_381_G2 &Q);
